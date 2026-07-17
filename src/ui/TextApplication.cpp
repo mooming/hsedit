7ea00f928affe74949f26d0a6439cfe9a8acbf5a
Hansol Park <mooming.go@gmail.com>
2026-07-17 19:28:40 +0900

    Add .clang-format to src/

---
+++ src/ui/TextApplication.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Hansol Park on 2026. 7. 17..
+//
+
+#include "TextApplication.h"
+
+namespace hs {
+} // hs
---
+++ src/ui/TextApplication.h
@@ -0,0 +1,15 @@
+//
+// Created by Hansol Park on 2026. 7. 17..
+//
+
+#pragma once
+
+namespace hs
+{
+
+class TextApplication
+{
+
+};
+
+} // namespace hs
